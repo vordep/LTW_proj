71c366ddecb4bb14900012acf3589093e130911e
vordep <j.jofe2@hotmail.com>
2015-11-23 18:36:33 +0000

    sign up

---
+++ templates/signup.php
@@ -0,0 +1,12 @@
+</head>
+<body>
+    <form action="signup.php" method="post">
+    <label for="inputUserName"> </label>
+    <input type="username" name="inputUserName" placeholder="User name " required autofocus>
+    <label for="inputPassword"> </label>
+    <input type="password" name= "inputPassword" placeholder="Password" required>
+    <label for="inputconfirmPassword"> </label>
+    <input type="password" name="inputconfirmPassword" placeholder="password" required>
+    <input type="submit" placeholder="submit">
+    </form>
+</body>
